5c56ce0d69b010a048b2df5fc0c3daeb9dc6f392
Abigail Watson <awatson1978@gmail.com>
2016-04-03 14:07:07 -0700

    ThemingUiTests

--- tests/gagarin/ThemingUiTests.js
+++ tests/gagarin/ThemingUiTests.js
@@ -1,8 +1,8 @@
-describe('clinical:static-pages', function () {
+describe('clinical:theming-ui', function () {
   var server = meteor();
   var client = browser(server);
 
-  it('Static pages exist on client.', function () {
+  it('Theming pages should exist on client.', function () {
     return client.execute(function () {
 
       expect(Template.themingActionButtons).to.exist();
